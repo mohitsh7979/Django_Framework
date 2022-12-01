45a4ad2941d768b647623489e49a5007e02fdec3
Mohit <Mohitsh7979@gmail.com>
2022-12-01 14:35:03 +0530

    manatoone

--- manytoone/myproj/myapp/admin.py
+++ manytoone/myproj/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['post_Title','post_content','post_date','user']
--- manytoone/myproj/myapp/models.py
+++ manytoone/myproj/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)  # cascade method if we want to delete my post with my page not user 
+    # user=models.ForeignKey(User,on_delete=models.PROTECT)  # protect method when we applid when if we want to delete my post with post user 
+    # user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True) # Null method we used when we want that user is detelet but post is stay
+    post_Title=models.CharField(max_length=100)
+    post_content=models.CharField(max_length=100)
+    post_date=models.DateTimeField()
+    
+
+
